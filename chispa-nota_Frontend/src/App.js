02f8eafa20cda883a5543dba8376c1f55fdcd5c3
Josue <josue7pedro@gmail.com>
2024-04-23 06:42:07 -0400

    frontend

--- chispa-nota_Frontend/src/App.js
+++ chispa-nota_Frontend/src/App.js
@@ -1,10 +1,12 @@
-
-import './App.css';
+import React from 'react';
+import './App.css'; // Importa los estilos CSS para la aplicación
+import MainLayout from './componentes/MainLayout'; // Importa el componente MainLayout
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      {/* Renderiza el componente MainLayout */}
+      <MainLayout />
     </div>
   );
 }
