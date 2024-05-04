8a0f4cd816167a2d03f9962e9d1709a1171976f2
Faxterefalt <azugafabian321@gmail.com>
2024-05-04 11:56:50 -0400

    Espacio para crear sala de pizarra

---
+++ chispa-nota_Frontend/src/componentes/Formas/index.jsx
@@ -0,0 +1,17 @@
+import CreateRoomForm from "./CrearSala";
+
+import "./index.css";
+const Forms = ({uuid, socket, setUser}) =>{
+  return(
+    <div className="row h-100 pt-5">
+          <div className="col-md-4 mt-5 form-box p-5 border  border-primary rounded-2 mx-auto d-flex flex-column align-items-center justify ">
+            <h1 className="text-primary fw-bold">Crear Sala</h1>
+            <CreateRoomForm uuid={uuid} socket={socket} setUser={setUser} />
+          </div>
+
+        </div>
+  );
+
+};
+
+export default Forms;
