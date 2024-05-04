07e17d6719ffbcf08254b42afb0e13ce76d48a2e
Faxterefalt <azugafabian321@gmail.com>
2024-05-04 12:05:17 -0400

    Espacio para unirse a sala de pizarra

---
+++ chispa-nota_Frontend/src/componentes/Formas/UnirseSala/index.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const JoinRoomForm = ({uuid, socket, setUser}) =>{
+
+
+  const[roomId, setRoomId]=useState("");
+  const[name, setName]=useState("");
+
+  const navigate=useNavigate();
+
+  const handleRoomJoin = (e) =>{
+    e.preventDefault();
+    const roomData={
+      name,
+      roomId,
+      userId: uuid(),
+      host: false,
+      presenter: false
+    };
+    setUser(roomData);
+    navigate(`/${roomId}`);
+    console.log(roomData);
+    socket.emit("userJoined", roomData);
+    
+  }
+return(
+  <form className='form col-md-12 mt-5'>
+        <div className="form-group">
+          <input 
+            type="text" 
+            className="form-control my-2"  
+            placeholder="Ingresa tu nombre"
+            value={name}
+            onChange={(e)=>setName(e.target.value)}
+            />
+        </div>
+        <div className="form-group">
+          
+            <input
+            type="text"
+            className="form-control my-2"
+            placeholder="Ingresar código de sala"
+            value={roomId}
+            onChange={(e)=>setRoomId(e.target.value)}
+            />
+            
+          </div>
+        
+        <button 
+        type="submit" 
+        onClick={handleRoomJoin}
+        className="btn btn-primary btn-block form-control">
+          Unirse a Sala
+          </button>
+     </form>
+)
+}
+
+export default JoinRoomForm;
--- chispa-nota_Frontend/src/componentes/Formas/index.jsx
+++ chispa-nota_Frontend/src/componentes/Formas/index.jsx
@@ -1,5 +1,5 @@
 import CreateRoomForm from "./CrearSala";
-
+import JoinRoomForm from "./UnirseSala";
 import "./index.css";
 const Forms = ({uuid, socket, setUser}) =>{
   return(
@@ -8,7 +8,10 @@ const Forms = ({uuid, socket, setUser}) =>{
             <h1 className="text-primary fw-bold">Crear Sala</h1>
             <CreateRoomForm uuid={uuid} socket={socket} setUser={setUser} />
           </div>
-
+          <div className="col-md-4 mt-5 form-box p-5 border  border-primary rounded-2 mx-auto d-flex flex-column align-items-center justify ">
+            <h1 className="text-primary fw-bold">Unirse a Sala</h1>
+            <JoinRoomForm uuid={uuid} socket={socket} setUser={setUser} />
+          </div>
         </div>
   );
 
