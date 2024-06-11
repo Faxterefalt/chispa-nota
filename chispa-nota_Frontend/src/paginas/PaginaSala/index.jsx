d6c3eab25ddf00ad3793fb58e9d15b11cf18c49f
Faxterefalt <azugafabian321@gmail.com>
2024-06-11 19:08:14 -0400

    hacer+deshacer+limpiar+scrollbar timeline

--- chispa-nota_Frontend/src/paginas/PaginaSala/index.jsx
+++ chispa-nota_Frontend/src/paginas/PaginaSala/index.jsx
@@ -8,15 +8,15 @@ function RoomPage({ user, socket, users }) {
   const [userList, setUsers] = useState([]);
   const [userActions, setUserActions] = useState([]);
 
-    const canvasRef = useRef(null);
-    const ctxRef = useRef(null);
-  
-    const [tool, setTool]=useState("pencil");
-    const [color, setColor]=useState("black");
-    const [elements, setElements]=useState([]);
-    const [history, setHistory]=useState([]);
-    const [openedUserTab, setOpenedUserTab]=useState(false);
-    const [openedChatTab, setOpenedChatTab]=useState(false);
+  const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
+
+  const [tool, setTool] = useState("pencil");
+  const [color, setColor] = useState("black");
+  const [elements, setElements] = useState([]);
+  const [history, setHistory] = useState([]);
+  const [openedUserTab, setOpenedUserTab] = useState(false);
+  const [openedChatTab, setOpenedChatTab] = useState(false);
 
   useEffect(() => {
     socket.on("onlineUsers", (numUsers) => {
@@ -122,32 +122,32 @@ function RoomPage({ user, socket, users }) {
         type="button" 
         className="btn btn-dark"
         style={{
-          display:"block",
-          position:"absolute",
-          top:"5%",
-          left:"3%", 
-          height:"40px",
-          width:"100px"
+          display: "block",
+          position: "absolute",
+          top: "5%",
+          left: "3%", 
+          height: "40px",
+          width: "100px"
         }}
-        onClick={()=>setOpenedUserTab(true)}
-        >
-          Usuarios
+        onClick={() => setOpenedUserTab(true)}
+      >
+        Usuarios
       </button>
 
       <button 
         type="button" 
         className="btn btn-primary"
         style={{
-          display:"block",
-          position:"absolute",
-          top:"5%",
-          left:"10%", 
-          height:"40px",
-          width:"100px"
+          display: "block",
+          position: "absolute",
+          top: "5%",
+          left: "10%", 
+          height: "40px",
+          width: "100px"
         }}
-        onClick={()=>setOpenedChatTab(true)}
-        >
-          Chat
+        onClick={() => setOpenedChatTab(true)}
+      >
+        Chat
       </button>
 
       {openedUserTab && (
@@ -177,115 +177,119 @@ function RoomPage({ user, socket, users }) {
               ))}
             </div>
           </div>
-        )
-      }
+        </div>
+      )}
 
-      {
-        openedChatTab&&(
-          <Chat setOpenedChatTab={setOpenedChatTab} socket={socket}/>
-        )
-      }
+      {openedChatTab && (
+        <Chat setOpenedChatTab={setOpenedChatTab} socket={socket}/>
+      )}
+
       <h1 className='text-center py-5'>
         Pizarra Compartida
         <span className="text-primary"> [Usuarios en línea : {users.length}]</span>
-        </h1>
-        {
-          user?.presenter &&(
-            <div className="col-md-10 mx-auto px-5 mt-4 mb-5 d-flex align-items-center justify-content-center">
+      </h1>
+
+      <div className="col-md-10 mx-auto px-5 mt-4 mb-5 d-flex align-items-center justify-content-center">
         <div className="d-flex col-md-4 justify-content-between gap-1">
           <div className="d-flex gap-1 align-items-center">
-            <label htmlfor="pencil">Lápiz</label>
+            <label htmlFor="pencil">Lápiz</label>
             <input 
-            type="radio" 
-            name="tool" 
-            id="pencil"
-            checked ={tool === "pencil"}
-            value="pencil" 
-            className="mt-1"
-            onChange={(e)=>setTool(e.target.value)}
+              type="radio" 
+              name="tool" 
+              id="pencil"
+              checked={tool === "pencil"}
+              value="pencil" 
+              className="mt-1"
+              onChange={(e) => setTool(e.target.value)}
             />
           </div>          
-  
+
           <div className="d-flex gap-1 align-items-center">
-          <label htmlfor="line">Línea</label>
-          <input 
-            type="radio" 
-            name="tool" 
-            id="line"
-            checked ={tool === "line"}
-            value="line" 
-            className="mt-1"
-            onChange={(e)=>setTool(e.target.value)}
-          />
+            <label htmlFor="line">Línea</label>
+            <input 
+              type="radio" 
+              name="tool" 
+              id="line"
+              checked={tool === "line"}
+              value="line" 
+              className="mt-1"
+              onChange={(e) => setTool(e.target.value)}
+            />
           </div>
-  
+
           <div className="d-flex gap-1 align-items-center">
-          <label htmlfor="rect">Rectángulo</label>
-          <input 
-            type="radio" 
-            name="tool" 
-            id="rect"
-            checked ={tool === "rect"}
-            value="rect" 
-            className="mt-1"
-            onChange={(e)=>setTool(e.target.value)}
-          />
+            <label htmlFor="rect">Rectángulo</label>
+            <input 
+              type="radio" 
+              name="tool" 
+              id="rect"
+              checked={tool === "rect"}
+              value="rect" 
+              className="mt-1"
+              onChange={(e) => setTool(e.target.value)}
+            />
           </div>
         </div>
         <div className="col-md-2 mx-auto">
           <div className="d-flex align-items-center">
             <label htmlFor="color">Selecciona un Color: </label>
             <input 
-            type="color" 
-            id="color" 
-            className="mt-1 ms-2"
-            value={color}
-            onChange={(e)=>setColor(e.target.value)}
+              type="color" 
+              id="color" 
+              className="mt-1 ms-2"
+              value={color}
+              onChange={(e) => setColor(e.target.value)}
             />
-  
           </div>
         </div>
-          <div className="col-md-3 d-flex gap-2">
-            <button className="btn btn-primary mt-1"
+        <div className="col-md-3 d-flex gap-2">
+          <button 
+            className="btn btn-primary mt-1"
             disabled={elements.length === 0}
-            onClick={()=> deshacer()}
-  
-            >Deshacer</button>
-            <button className="btn btn-outline-primary mt-1"
+            onClick={() => deshacer()}
+          >
+            Deshacer
+          </button>
+          <button 
+            className="btn btn-outline-primary mt-1"
             disabled={history.length < 1}
-            onClick={()=>rehacer()}
-            >
-              Rehacer</button>
-          </div>
-<div className="col-md-2">
-    <button className="btn btn-danger" onClick={handleClearCanvas}>Limpiar Canvas</button>
-    
-</div>
+            onClick={() => rehacer()}
+          >
+            Rehacer
+          </button>
+        </div>
+        <div className="col-md-2">
+          <button 
+            className="btn btn-danger" 
+            onClick={() => {
+              handleClearCanvas();
+              socket.emit("clearCanvas"); 
+            }}
+          >
+            Limpiar Canvas
+          </button>
+        </div>
+        <button 
+          className="btn ml-2" 
+          style={{backgroundColor: 'purple', color: 'white'}}
+          onClick={() => {
+            Swal.fire({
+              title: '¿Quieres exportar la imagen a tu espacio de trabajo?',
+              showDenyButton: true,
+              confirmButtonText: `Exportar`,
+              denyButtonText: `Seguir Editando`,
+            }).then((result) => {
+              if (result.isConfirmed) {
+                handleExport();
+                window.location.href = '/mainpage';
+              }
+            })
+          }}
+        >
+          Mover a Principal
+        </button>
+      </div>
 
-<button 
-  className="btn ml-2" 
-  style={{backgroundColor: 'purple', color: 'white'}}
-  onClick={() => {
-    Swal.fire({
-      title: '¿Quieres exportar la imagen a tu espacio de trabajo?',
-      showDenyButton: true,
-      confirmButtonText: `Exportar`,
-      denyButtonText: `Seguir Editando`,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        handleExport();
-        window.location.href = '/mainpage';
-      }
-    })
-  }}
->
-  Mover a Principal
-</button>
-            </div>
-          )
-        }
-  
-  
       <div className="col-md-10 mx-auto mt-1 canvas-box">
         <WhiteBoard 
           canvasRef={canvasRef} 
@@ -296,12 +300,10 @@ function RoomPage({ user, socket, users }) {
           tool={tool}
           user={user}
           socket={socket}
-          
-        
         />
       </div>
     </div>
-    )
-  }
-  
-  export default RoomPage;
+  );
+}
+
+export default RoomPage;
